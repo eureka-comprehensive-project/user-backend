9c73e2d599e1b18264ff0fab8d0d8bf9d1bda164
hyunnk <jhyunnk30@gmail.com>
2025-06-11 10:20:54 +0900

    feat: 사용자 기록 엔티티 생성 및 사용자 데이터 기록 uri 변경

---
+++ src/main/java/com/comprehensive/eureka/user/entity/UserPlanRecord.java
@@ -0,0 +1,40 @@
+package com.comprehensive.eureka.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_plan_record")
+public class UserPlanRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_plan_record_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "plan_benefit_id", nullable = false)
+    private Long planBenefitId;
+
+    @Column(name = "contract_date")
+    private LocalDate contractDate;
+
+    @Column(name = "expiration_date")
+    private LocalDate expirationDate;
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+}
