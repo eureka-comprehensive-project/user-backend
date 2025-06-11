9a16f847c2d162dcc80b9fb62e6e29b0a78ead21
hyunnk <jhyunnk30@gmail.com>
2025-06-11 13:24:51 +0900

    fix: 사용자 등록 내용 수정

--- src/main/java/com/comprehensive/eureka/user/exception/ErrorCode.java
+++ src/main/java/com/comprehensive/eureka/user/exception/ErrorCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    USER_NOT_FOUND(50000, "USER_NOT_FOUND", "해당 사용자를 찾을 수 없습니다.");
+    USER_NOT_FOUND(50000, "USER_NOT_FOUND", "해당 사용자를 찾을 수 없습니다."),
+    EMAIL_ALREADY_EXISTS(50001, "EMAIL_ALREADY_EXISTS", "중복된 email 입니다.");
 
     private final int code;
     private final String name;
